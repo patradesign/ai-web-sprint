4180f0f8df41b8f05b039e3e3f7ead67a9f32eea
Doris Patra <patra-lokal@MacBookPro-Dez-2021-patradesign.fritz.box>
2026-04-27 23:48:45 +0200

    Update splash: bold orange font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black">
-      <h1 className="text-fuchsia-500 text-5xl font-light tracking-widest uppercase font-mono">
+      <h1 className="text-orange-500 text-5xl font-black tracking-widest uppercase font-mono">
         Brand Design
       </h1>
     </div>
