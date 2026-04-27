542eeaae9c755463e84f5e118032f843b4757c71
Doris Patra <patra-lokal@MacBookPro-Dez-2021-patradesign.fritz.box>
2026-04-27 23:46:18 +0200

    Update splash: Brand Design in magenta mono font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black">
-      <h1 className="text-white text-5xl font-light tracking-widest uppercase">
-        Design is everything
+      <h1 className="text-fuchsia-500 text-5xl font-light tracking-widest uppercase font-mono">
+        Brand Design
       </h1>
     </div>
   );
